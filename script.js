ff6ae87ce18111c8d469cc385ac8cf2c388d2ac8
Majdi Saibi <saibimajdi@outlook.com>
2017-05-18 16:02:11 +0200

    remove redirection

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 
-window.location.href = "https://www.linkedin.com/mynetwork/";
-
 setInterval(function(){
 	
     scrollBy(0, window.outerHeight - 10);
-	
-		$('.mn-person-card__person-btn-ext').each(function(index, item){  
-            //console.warn(val + ": " + index);
-			setTimeout(function(){ $(this).click(); }, 200);
-            //var person = document.getElementsByClassName('mn-person-info__name')[index].innerText;
-			console.log('invitation sent'); 
-		}); 
+    $('.mn-person-card__person-btn-ext').each(function(){  
+	setTimeout(function(){ $(this).click(); }, 200);
+	console.log('invitation sent'); 
+    }); 
 		
 }, 5000);
