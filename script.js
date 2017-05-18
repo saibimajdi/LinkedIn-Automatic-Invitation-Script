ce9baadda204ad2af402e8c2b07439ce5ecd24eb
Majdi Saibi <saibimajdi@outlook.com>
2017-05-18 16:05:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+var screenHeight = window.outerHeight - 10;
+
+// change if you have a bad network connection
+var timeBetweenTwoRequest = 200; // 0.2 seconds
+var timeToScrollAndLoadNewRequests = 5000; // 5 seconds
 
 setInterval(function(){
 	
-    scrollBy(0, window.outerHeight - 10);
+    scrollBy(0, screenHeight);
     $('.mn-person-card__person-btn-ext').each(function(){  
-	setTimeout(function(){ $(this).click(); }, 200);
+	setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
 	console.log('invitation sent'); 
     }); 
 		
-}, 5000);
+}, timeToScrollAndLoadNewRequests);
