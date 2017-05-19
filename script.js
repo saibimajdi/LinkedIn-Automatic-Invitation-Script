ac9564a35c3bd41ab19f2f3198fee08cf3faeb50
Saibi Majdi <saibimajdi@outlook.com>
2017-05-19 17:06:25 +0200

    uncomment ui2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var screenHeight = window.outerHeight - 10;
 
 // change if you have a bad network connection
 var timeBetweenTwoRequest = 200; // 0.2 seconds
-var timeToScrollAndLoadNewRequests = 3000; // 5 seconds
+var timeToScrollAndLoadNewRequests = 3000; // 3 seconds
 var requests = 0;
 
 
@@ -21,20 +21,20 @@ setInterval(function(){
 
 setTimeout(function(){
     setInterval(function(){
+        // UI 1
         $('.mn-person-card__person-btn-ext').each(function(){  
             //setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
             $(this).click();
-            //$(this).removeClass('mn-person-card__person-btn-ext');
             console.log('invitation sent'); 
         }); 
 
-        // $('.mn-pymk-list__action-container > .button-secondary-small').each(function(){  
-        //     //setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
-        //     $(this).click();
-        //     //$(this).removeClass('button-secondary-small');
-        //     console.log('invitation sent'); 
-        // });
+        // UI 2
+        $('.mn-pymk-list__action-container > .button-secondary-small').each(function(){  
+            //setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
+            $(this).click();
+            console.log('invitation sent'); 
+        });
 
-    }, timeToScrollAndLoadNewRequests * 5);
     requests = 0;
-}, timeToScrollAndLoadNewRequests * 5);
+    }, timeToScrollAndLoadNewRequests * 3);
+}, timeToScrollAndLoadNewRequests * 3);
