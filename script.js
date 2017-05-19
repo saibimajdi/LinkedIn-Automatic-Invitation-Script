7dec18ee16f4f551efe54d5c65612ce89d5d803d
Saibi Majdi <saibimajdi@outlook.com>
2017-05-19 16:29:47 +0200

    fix

--- script.js
+++ script.js
@@ -9,14 +9,12 @@ var requests = 0;
 setInterval(function(){
 	
     scrollBy(0, screenHeight);
-
     requests += 1;
-
     if(requests > 5){
         console.log("STOPED");
         return;
     }
-		
+
 }, timeToScrollAndLoadNewRequests);
 
 
@@ -26,14 +24,17 @@ setTimeout(function(){
         $('.mn-person-card__person-btn-ext').each(function(){  
             //setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
             $(this).click();
+            //$(this).removeClass('mn-person-card__person-btn-ext');
             console.log('invitation sent'); 
         }); 
 
-        $('.mn-pymk-list__action-container > .button-secondary-small').each(function(){  
-            //setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
-            $(this).click();
-            console.log('invitation sent'); 
-        });
+        // $('.mn-pymk-list__action-container > .button-secondary-small').each(function(){  
+        //     //setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
+        //     $(this).click();
+        //     //$(this).removeClass('button-secondary-small');
+        //     console.log('invitation sent'); 
+        // });
 
     }, timeToScrollAndLoadNewRequests * 5);
+    requests = 0;
 }, timeToScrollAndLoadNewRequests * 5);
