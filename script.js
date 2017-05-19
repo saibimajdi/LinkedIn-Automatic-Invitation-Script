0d40ae6ffdcb193291d2b1f7cb2d27a794d0907a
Saibi Majdi <saibimajdi@outlook.com>
2017-05-19 15:41:53 +0200

    .

--- script.js
+++ script.js
@@ -9,21 +9,13 @@ var requests = 0;
 setInterval(function(){
 	
     scrollBy(0, screenHeight);
-<<<<<<< HEAD
+
     requests += 1;
 
     if(requests > 5){
         console.log("STOPED");
         return;
     }
-=======
-    $('.mn-person-card__person-btn-ext').each(function(){  
-	setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
-	console.log('invitation sent'); 
-    }); 
-	
-	
->>>>>>> a85a569c330a6c0386a90d606c60ca10565a20e6
 		
 }, timeToScrollAndLoadNewRequests);
 
@@ -38,7 +30,8 @@ setTimeout(function(){
         }); 
 
         $('.mn-pymk-list__action-container > .button-secondary-small').each(function(){  
-            setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
+            //setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
+            $(this).click();
             console.log('invitation sent'); 
         });
 
