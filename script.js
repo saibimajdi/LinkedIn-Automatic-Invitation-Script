9ad6f795e7821c80bd4f57d44e14f8680b8d8e84
Saibi Majdi <saibimajdi@outlook.com>
2017-05-19 15:32:27 +0200

    ..

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ var screenHeight = window.outerHeight - 10;
 
 // change if you have a bad network connection
 var timeBetweenTwoRequest = 200; // 0.2 seconds
-var timeToScrollAndLoadNewRequests = 5000; // 5 seconds
+var timeToScrollAndLoadNewRequests = 3000; // 5 seconds
+var requests = 0;
+
 
 setInterval(function(){
 	
     scrollBy(0, screenHeight);
-    $('.mn-person-card__person-btn-ext').each(function(){  
-	setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
-	console.log('invitation sent'); 
-    }); 
+    requests += 1;
+
+    if(requests > 5){
+        console.log("STOPED");
+        return;
+    }
 		
 }, timeToScrollAndLoadNewRequests);
+
+
+
+setTimeout(function(){
+    setInterval(function(){
+        $('.mn-person-card__person-btn-ext').each(function(){  
+            //setTimeout(function(){ $(this).click(); }, timeBetweenTwoRequest);
+            $(this).click();
+            console.log('invitation sent'); 
+        }); 
+    }, timeToScrollAndLoadNewRequests * 5);
+}, timeToScrollAndLoadNewRequests * 5);
